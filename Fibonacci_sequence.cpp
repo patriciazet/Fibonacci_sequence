5f2b95c8b56fded98931e12b2a9258a63e14d1a6
Patrycja Z <patrycja.zet3@gmail.com>
2022-07-08 22:07:39 +0200

    Initial commit.

---
+++ Fibonacci_sequence.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+class Fibonacci
+{
+private:
+    int sum, a, b;
+public: 
+    int generate(int n)
+    {
+        if (n <= 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return generate(n - 2) + generate(n - 1);
+        }
+    }
+
+    int iteratively(int n)
+    {
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        for (int i = 1; i <= n - 2; i++)
+        {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return sum;
+        
+    }
+};
+int main()
+{
+    Fibonacci fib;
+    std::cout << fib.iteratively(7);
+}
+
